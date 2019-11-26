d7f0f7a4ba5930ba6ad7af8ef2ca0d9f9ba502ff
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-11-26 06:30:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ end
 def monopoly_with_second_tier
 	monopoly = {
 	  :railroads => {
-	    :pieces => 4,  :names => {}, :rent_in_dollars => {}
+	    :pieces => 4,  :names => {:reading_railroad => {}, :pennsylvania_railroad => {}, :b_o_railroad => {}, shortline => {}}, :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :third_pieces_owned => 100, :four_pieces_owned => 200 }
 	   }
 	  
 	}
@@ -24,7 +24,13 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  
+  	monopoly = {
+	  :railroads => {
+	    :pieces => 4,  :names => {}, :rent_in_dollars => {}
+	   }
+	  
+	}
+
 	
 
 end
