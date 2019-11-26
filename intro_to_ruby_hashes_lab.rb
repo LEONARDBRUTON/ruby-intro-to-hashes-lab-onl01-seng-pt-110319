cb93b037210179ecb2f5b6247abe97fdf3e83f99
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-11-26 06:42:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,13 +30,10 @@ def monopoly_with_third_tier
 	   }
 	  
 	}
-
-	
-
 end
 
 def monopoly_with_fourth_tier
-	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
+	
 
 end
 
